9f691719756d9cdb1f790cadf57926340b97fd33
Anya Fadeeva <anyasimba@yandex.ru>
2021-02-03 21:09:18 +0300

    Fixes

--- src/common/HakunaMatata/index.ts
+++ src/common/HakunaMatata/index.ts
@@ -5,20 +5,18 @@ globalify(_)
 console.log('No worries!')
 
 declare global {
+    const collectGarbage: typeof _.collectGarbage
     type IHakunaMatata = _.IHakunaMatata
     const HakunaMatata: typeof _.HakunaMatata & typeof _.HakunaMatataClass
     const Effect: typeof _.Effect & typeof _.EffectClass
     type PureEffect = _.PureEffect
     const asEffect: typeof _.asEffect
     const Self: typeof _.Self
-
     function useRelation<T>(
         initialValue: T | ((subject: T) => void),
         set?: (subject: T) => void
     ): [() => T | null, (newValue: T | null) => void]
-
     type Relation = _.Relation
     const asRelation: typeof _.asRelation
-
     function useEffect(cb: () => (...args: any[]) => void): void
 }
