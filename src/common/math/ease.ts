9693f326efc3716ad0162ae2b35e260a8e11f69f
Anna Fadeeva <anyasimba@yandex.ru>
2021-07-21 14:59:44 +0300

    js doc

--- src/common/math/ease.ts
+++ src/common/math/ease.ts
@@ -7,6 +7,14 @@ declare global {
 }
 
 namespace _ {
+    /**
+     * Ease функция (in, out quad)
+     * @param {Number} t время, 0 - продолжительность
+     * @param {Number} b начало
+     * @param {Number} c длина
+     * @param {Number} d duration, продолжительность
+     * @returns {Number} Returns the value of x for the equation.
+     */
     export function easeInOutQuad(t: number, b: number, c: number, d: number): number {
         t /= d / 2
         if (t < 1) {
